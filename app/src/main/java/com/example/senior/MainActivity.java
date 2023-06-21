5920361e6bddd7bec4dd75ebfeac0cc485c04290
Jeong SuHyeon <jsh00325@naver.com>
2023-06-21 20:15:39 +0900

    initial

---
+++ app/src/main/java/com/example/senior/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.senior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // give permission to use microphone
+        if (checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{android.Manifest.permission.RECORD_AUDIO}, 1);
+        }
+
+
+    }
+}
