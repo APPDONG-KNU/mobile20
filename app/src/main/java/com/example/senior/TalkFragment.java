8650ff178b035f33f3325e50fc0be539c11c84a9
dn352 <pathemata>
2023-06-22 12:49:29 +0900

    chat gpt inserted

--- app/src/main/java/com/example/senior/TalkFragment.java
+++ app/src/main/java/com/example/senior/TalkFragment.java
@@ -2,34 +2,104 @@ package com.example.senior;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.microsoft.cognitiveservices.speech.SpeechConfig;
 import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
 import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class TalkFragment extends Fragment {
     private View view;
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS).build();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_talk, container, false);
-
         return view;
     }
+
+    void callAPI(String question){
+
+        //okhttp
+        JSONObject jsonBody = new JSONObject();
+        try{
+            jsonBody.put("model","text-davinci-003");
+            jsonBody.put("prompt",question);
+            jsonBody.put("max_tokens",4000);
+            jsonBody.put("temperature",0);
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        RequestBody body =RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(body)
+                .build();
+
+
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Toast.makeText(getContext(),"Failed to load response due to "+ e.getMessage() , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+
+                    JSONObject jsonObject =null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        Toast.makeText(getContext(), result.trim(), Toast.LENGTH_SHORT).show();
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    Toast.makeText(getContext(), "Failed to load response due to " + response.body().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 
 
     // start recognition
     public void startRecognition() {
+
+        String question ="";
         // speech config listener
         SpeechConfig speechConfig = SpeechConfig.fromSubscription("84e519c9a7a243c9926aae596b677979", "koreacentral");
         speechConfig.setSpeechRecognitionLanguage("ko-KR");
@@ -42,13 +112,17 @@ public class TalkFragment extends Fragment {
             SpeechRecognitionResult result = task.get();
             Log.i("SpeechSDKDemo", result.getText());
             Log.d("SpeechSDKDemo", result.toString());
+            question = result.getText();
         } catch (InterruptedException e) {
             e.printStackTrace();
+            question =null;
         } catch (ExecutionException e) {
             e.printStackTrace();
+            question =null;
         }
 
         // close the reco
         reco.close();
+        callAPI(question);
     }
 }
