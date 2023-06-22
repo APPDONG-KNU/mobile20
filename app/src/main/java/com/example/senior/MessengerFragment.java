1bdf8f18e165454d233b81b89e89ef0dde0770b0
Jeong SuHyeon <jsh00325@naver.com>
2023-06-23 05:28:39 +0900

    오류 수정

--- app/src/main/java/com/example/senior/MessengerFragment.java
+++ app/src/main/java/com/example/senior/MessengerFragment.java
@@ -13,8 +13,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.intuit.sdp.BuildConfig;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
